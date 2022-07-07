704aad9bab8934e76f64a2317c754106eccc3c8c
Adriano Barbosa <adrianobarbosa95@gmail.com>
2022-07-07 14:18:38 -0300

    web

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,18 @@
 <?php
 
+ 
+use App\Http\Controllers\BeneficiadoController;
+use App\Http\Controllers\ListaController;
+use App\Models\Lista;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('home');
 })->name('index');
 
-Route::get('/lista', function () {
+Route::get('/listas', function () {
     return view('lista');
 })->middleware(['auth'])->name('dashboard');
 
@@ -28,6 +23,34 @@ Route::middleware('guest')->group(
     function () {
 
 
+  
+
+
+
+        Route::get('/dashboard/{lista?}', function (Lista $lista = null) {
+            $lista = Lista::all();
+            return view('lista', ['listas' => $lista]);
+        })->name('dashboard');
+
+        Route::get('/listas/{lista?}', function (Lista $lista = null) {
+            $lista = Lista::all();
+            return view('lista', ['listas' => $lista]);
+        })->name('listas');
+
+
+
+        Route::get('/lista', function () {
+            return view('cadastrar-lista');
+        })->name('lista');
+        Route::post('/cadastrarLista', [ListaController::class, 'store'])->name('cadastrar.lista');
+        Route::get('/beneficiado/{lista}', [BeneficiadoController::class, 'index'])->name('beneficiado');
+
+        Route::post('/buscarbeneficiado', [BeneficiadoController::class, 'show'])->name('buscar.beneficiado');
+
+        Route::get('/cadastrar/{lista}/{beneficiado}', function (Request $r, $lista, $beneficiado) {
+
+            return view('cadastrar', ['lista' => $lista, 'beneficiado' => $beneficiado]);
+        })->name('cadastro.beneficiado');
 
 
 
@@ -35,11 +58,9 @@ Route::middleware('guest')->group(
 
 
 
+        Route::post('/cadastroBeneficiado', [BeneficiadoController::class, 'store'])->name('cadastrar.beneficiado');
 
 
-
-
-        
     });
 
-require __DIR__.'
+require __DIR__.'/auth.php';
